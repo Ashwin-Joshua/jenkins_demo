32eec532358ff2a297de477342084adc8ff2bac7
Ashwin.J <Ashwin.J@EXIND3010-52.allfon.in>
2022-03-21 12:47:19 +0530

    req-102 get by dept endpoint added

--- jenkins-git-maven-docker-build/src/main/java/com/example/demo/controllers/TeacherController.java
+++ jenkins-git-maven-docker-build/src/main/java/com/example/demo/controllers/TeacherController.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +17,17 @@ public class TeacherController {
 		List<String> list =Arrays.asList("Ash","San","Abd");
 		return list.toString();
 	}
+	
+	@GetMapping("/teachers/{dept}")
+	public String getTeachers(@PathVariable String dept) {
+		List<String> list =Arrays.asList("Ash","San","Abd");
+		
+		if(dept.equals("ece")) {
+			list= Arrays.asList("Ashwin","Ajay","boiii");
+		}
+		
+		return list.toString();
+	}
+	
 	
 }
